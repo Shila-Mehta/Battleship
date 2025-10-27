e005982875125538f80c9cd1424e5aeb44c98c5d
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-10-27 18:43:14 +0500

    fixed linkdin url in readme

--- src/index.js
+++ src/index.js
@@ -2,29 +2,21 @@ import { ThemeManager } from "./Components/ThemeManager";
 import { GameService } from "./Services/GameServices";
 import "./Styles/main.css";
 
+document.addEventListener('DOMContentLoaded', () => {
+  ThemeManager.applyRandomTheme();
 
-document.addEventListener('DOMContentLoaded',()=>{
-ThemeManager.applyRandomTheme();
-
- document.querySelector('.legend-toggle').addEventListener('click',()=>{
+  document.querySelector('.legend-toggle').addEventListener('click', () => {
     document.querySelector('.legend-panel').classList.toggle('hidden');
+  });
 
-  })
-
- document.querySelector('.guide-book').addEventListener('click',()=>{
+  document.querySelector('.guide-book').addEventListener('click', () => {
     document.querySelector('.guide-container').classList.toggle('hidden');
- })
+  });
 
-
- document.getElementById('theme-toggle').addEventListener('click',()=>{
+  document.getElementById('theme-toggle').addEventListener('click', () => {
     ThemeManager.toggleDarkLight();
-})
+  });
 
-GameService.startGame();
-
-
-})
-
-
-
-
+  // Initialize game & listeners
+  GameService.initializeGame();
+});
